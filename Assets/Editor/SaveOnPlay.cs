b126b29e384371d5b8a10c9ab69329ea3b4487e2
Casey Young <elementcy@gmail.com>
2016-02-24 13:29:52 -0600

    Added Base 2D Project

---
+++ Assets/Editor/SaveOnPlay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class SaveOnPlay
+{
+	static SaveOnPlay()
+	{
+
+		EditorApplication.playmodeStateChanged = () =>
+		{
+
+			if( EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying )
+			{
+				UnityEngine.SceneManagement.Scene scene = EditorSceneManager.GetActiveScene();
+				Debug.Log( "Saving scene before entering Play mode: " + scene.name );
+				
+				EditorSceneManager.SaveScene(scene);
+				EditorApplication.SaveAssets();
+			}
+
+		};
+
+	}
+}
